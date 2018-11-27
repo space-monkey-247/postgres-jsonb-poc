1481cfdfa6ffd74c46cb8cf8f49ec64a8f04eeb3
Bogdan Buzac <bogdanbuzac@gmail.com>
2018-11-27 21:21:38 +0200

    add

---
+++ src/main/java/com/hudsonmx/postgresdemo/PostgresDemoApplication.java
@@ -0,0 +1,12 @@
+package com.hudsonmx.postgresdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgresDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgresDemoApplication.class, args);
+	}
+}
---
+++ src/main/java/com/hudsonmx/postgresdemo/model/AuditModel.java
@@ -0,0 +1,16 @@
+package com.hudsonmx.postgresdemo.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(
+        value = {"createdAt", "updatedAt"},
+        allowGetters = true
+)
+public abstract class AuditModel {
+}
